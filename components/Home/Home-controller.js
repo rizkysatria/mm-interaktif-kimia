e2ad10286d7e0d67218afad136d65700140ec95a
rizkysatrian <rizkysatrian@gmail.com>
2017-09-24 22:36:42 +0700

    update ui

--- components/Home/Home-controller.js
+++ components/Home/Home-controller.js
@@ -8,6 +8,12 @@
 		  homeViewCtrlVM.dataComplete = null;
       homeViewCtrlVM.penjelasanMateri = null; 
       homeViewCtrlVM.buttonMenuUtamaCLicked = buttonMenuUtamaCLicked;
+      homeViewCtrlVM.loadTemplate = loadTemplate;
+      homeViewCtrlVM.loadTemplateSubJudul = loadTemplateSubJudul;
+      homeViewCtrlVM.isOpen = false;
+      homeViewCtrlVM.isOpenSubmenu = false;
+      homeViewCtrlVM.selectedMenuUtama = null;
+
 		  getDataFromJson();
 
       function getDataFromJson() {
@@ -17,10 +23,42 @@
       }
 
       function buttonMenuUtamaCLicked(id) {
+        homeViewCtrlVM.selectedMenuUtama = id;
         for (var i = 0; i < homeViewCtrlVM.dataComplete.length; i++) {
           if (homeViewCtrlVM.dataComplete[i].id === id) {
             homeViewCtrlVM.penjelasanMateri = homeViewCtrlVM.dataComplete[i];
           }
+        }
+      }
+
+      function loadTemplate(template, isOpen) {
+        if (!isOpen) {
+          homeViewCtrlVM.isOpenSubmenu = false;
+        }
+
+        if (template && isOpen && !homeViewCtrlVM.isOpenSubmenu || !isOpen && !homeViewCtrlVM.isOpenSubmenu ) {
+          var container = $('.dynamic-content');
+          container.html("");
+          console.log(container.html())
+          container.html(template);
+          console.log(container.html())
+          var newScope = angular.element(container).scope();
+          var compile = angular.element(container).injector().get('$compile');
+          compile($(container).contents())(newScope);
+        }
+      }
+
+      function loadTemplateSubJudul(template, isOpen) {
+        homeViewCtrlVM.isOpenSubmenu = isOpen;
+         if (template && isOpen) {
+          var container = $('.dynamic-content');
+          container.html("");
+          console.log(container.html())
+          container.html(template);
+          console.log(container.html())
+          var newScope = angular.element(container).scope();
+          var compile = angular.element(container).injector().get('$compile');
+          compile($(container).contents())(newScope);
         }
       }
     }
